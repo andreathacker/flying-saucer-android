c5c1305c7f186cde01d130da37806f431ba318fa
David Thacker <davidargylethacker@gmail.com>
2015-05-21 19:19:13 -0500

    Models created. Implementation for Beer model given

---
+++ FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/data/model/Beer.java
@@ -0,0 +1,20 @@
+package com.dat.flyingsaucer.data.model;
+
+/**
+ * Created by davidthacker on 5/21/15.
+ */
+public class Beer {
+
+    public String title;
+    public String description;
+    public String style;
+    public String container;
+    public String brewer;
+    public String city;
+    public String country;
+
+    public Beer(){
+        super();
+    }
+
+}
---
+++ FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/data/model/Brewery.java
@@ -0,0 +1,7 @@
+package com.dat.flyingsaucer.data.model;
+
+/**
+ * Created by davidthacker on 5/21/15.
+ */
+public class Brewery {
+}
