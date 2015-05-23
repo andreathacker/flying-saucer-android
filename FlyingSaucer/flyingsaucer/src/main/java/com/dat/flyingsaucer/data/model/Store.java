dab6045b00adeb0dc68f4446f73d72cf4c7e2b46
David Thacker <davidargylethacker@gmail.com>
2015-05-23 12:25:37 -0500

    Store and Location models added

---
+++ FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/data/model/Store.java
@@ -0,0 +1,16 @@
+package com.dat.flyingsaucer.data.model;
+
+/**
+ * Created by davidthacker on 5/23/15.
+ */
+public class Store {
+
+    public String slug;
+    public String name;
+    public Location location;
+
+    public Store() {
+        super();
+    }
+
+}
