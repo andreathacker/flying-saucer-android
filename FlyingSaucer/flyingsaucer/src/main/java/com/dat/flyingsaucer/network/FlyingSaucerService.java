f5667fcd9dad6bf80a1569026fa0fa77e98d391d
David Thacker <davidargylethacker@gmail.com>
2015-05-23 15:12:57 -0500

    The retrofit service interface is completed.

--- FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/network/FlyingSaucerService.java
+++ FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/network/FlyingSaucerService.java
@@ -1,6 +1,9 @@
 package com.dat.flyingsaucer.network;
 
 import com.dat.flyingsaucer.data.model.Beer;
+import com.dat.flyingsaucer.data.model.Store;
+
+import java.util.List;
 
 import retrofit.http.GET;
 import retrofit.http.Path;
@@ -11,6 +14,16 @@ import retrofit.http.Path;
 public interface FlyingSaucerService {
 
     @GET("/beers/{id}")
-    Beer getBeerFromId(@Path("{id}") String id);
+    Beer getBeerFromId(@Path("{id}") String beerId);
 
+    @GET("/stores")
+    List<Store> getAllStores();
+
+    @GET("/stores/{id}")
+    Store getStoreFromId(@Path("{id}") String storeId);
+
+    /* The params for lat/lon have not yet been provided. This call will be updated, once I figure
+    * out the correct format to provide the location to the api. */
+    @GET("/nearby")
+    List<Store> getNearbyStores();
 }
