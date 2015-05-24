1168e31ccb50485f7ee331ae492513d5569dd385
David Thacker <davidargylethacker@gmail.com>
2015-05-23 23:30:26 -0500

    Butterknife library added

--- FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/BeerListActivityFragment.java
+++ FlyingSaucer/flyingsaucer/src/main/java/com/dat/flyingsaucer/BeerListActivityFragment.java
@@ -2,44 +2,72 @@ package com.dat.flyingsaucer;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.dat.flyingsaucer.data.dao.BeerDao;
+import com.dat.flyingsaucer.data.dao.StoreBeerItemDao;
 import com.dat.flyingsaucer.data.dao.StoreDao;
 import com.dat.flyingsaucer.data.model.Beer;
+import com.dat.flyingsaucer.data.model.Store;
 import com.dat.flyingsaucer.data.model.StoreBeerItem;
 import com.dat.flyingsaucer.network.SaucerRestAdapter;
+import com.dat.flyingsaucer.network.SaucerService;
 
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.Callback;
+import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class BeerListActivityFragment extends Fragment {
 
+    public static final String TAG = "Retrofit";
+
     public BeerListActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_beer_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_beer_list, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        performApiCalls();
+    }
 
-        /* This is sample code to demonstrate that Retrofit has been
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick(R.id.refresh_button)
+    public void onClickRefresh(){
+        performApiCalls();
+    }
+
+    public void performApiCalls(){
+               /* This is sample code to demonstrate that Retrofit has been
         * correctly integrated into the project. */
         SaucerRestAdapter restAdapter = new SaucerRestAdapter();
-        restAdapter.getAllStores(new SaucerRestAdapter.RestCallback<StoreDao>() {
-            @Override
-            public void onSuccess(StoreDao dao) {
 
+        restAdapter.getBeer("2", new SaucerRestAdapter.RestCallback<BeerDao>() {
+            @Override
+            public void onSuccess(BeerDao dao) {
+                Log.d(TAG, "GetBeer success, /beer/:id");
             }
 
             @Override
@@ -48,10 +76,11 @@ public class BeerListActivityFragment extends Fragment {
             }
         });
 
-        restAdapter.getBeer("2", new SaucerRestAdapter.RestCallback<BeerDao>() {
-            @Override
-            public void onSuccess(BeerDao dao) {
 
+        restAdapter.getStoreBeerItems("nashville", new SaucerRestAdapter.RestCallback<StoreBeerItemDao>() {
+            @Override
+            public void onSuccess(StoreBeerItemDao dao) {
+                Log.d(TAG, "GetStoreBeerItems success, /stores/nashville/beers");
             }
 
             @Override
